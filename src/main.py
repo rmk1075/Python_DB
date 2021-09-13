390213726b886d8dd7a865495632b082f5464568
jamie <rmk1075@gmail.com>
2021-09-13 16:12:13 +0000

    update main source

--- src/main.py
+++ src/main.py
@@ -1,27 +1,6 @@
-import sqlalchemy as db
+import sqlalchemy
 
-name = 'init'
-password = 'init123!!'
-url = 'db' # database service name in docker-compose
-port = '3306'
-database = 'project'
-conn = f'mysql+pymysql://{name}:{password}@{url}:{port}/{database}'
+from src.models.user import User
 
-engine = db.create_engine(conn)
-connection = engine.connect()
-metadata = db.MetaData()
-
-table_name = 'test'
-table = db.Table(table_name, metadata, autoload=True, autoload_with=engine)
-
-print(f'table columns - {table.columns.keys()}\n')
-
-# select * from table
-query = db.select([table])
-print(f'query: {query}\n')
-
-# execute query
-result_proxy = connection.execute(query)
-result_set = result_proxy.fetchall()
-
-print(result_set)
+if __name__ == '__main__':
+    pass
